86b2943af9301792c36bc7a89fe0ceebb68d096b
Jayesh Saini <jayesh.saini.95@gmail.com>
2023-05-16 02:00:28 -0400

    fixed the get post initial error message

--- src/app/components/get-post/get-post.component.ts
+++ src/app/components/get-post/get-post.component.ts
@@ -9,8 +9,13 @@ import { IPost, PostService } from 'src/app/service/post.service';
 export class GetPostComponent {
   postId: number = 0;
   postData: any;
+  dataLoaded: boolean = false; // Flag to track if data has been loaded
 
   constructor(private postService: PostService) {}
+
+  onInit() {
+    this.postData = 1;
+  }
 
   // handling the get post submit
   onSubmit() {
@@ -18,10 +23,12 @@ export class GetPostComponent {
       this.postService.getPost(this.postId).subscribe({
         next: (data) => {
           this.postData = data;
+          this.dataLoaded = true; // Set the flag to true on API call
         },
         error: (error) => {
           console.log(error);
           this.postData = null; // Set postData to null when there is an error
+          this.dataLoaded = true; // Set the flag even when there is error to indicate data is loaded
         },
       });
     }
